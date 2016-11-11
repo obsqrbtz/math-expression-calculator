a6a60f5fd139b2fb78bbb8ba52c77bd4b76abcf1
Daniel Dada <doesdeos@gmail.com>
2016-11-12 00:31:10 +0300

    Made get_neutral_element() and get_operator_priority() private.

--- math-expression-calculator/calculator.h
+++ math-expression-calculator/calculator.h
@@ -20,6 +20,8 @@ class expression{
 		operations operation[OPERATIONS];
 		std::vector<float> operands_stack;
 		std::vector<std::string> operators_stack;
+		int get_operator_priority(std::string);
+		std::string get_neutral_element(std::string);
 		bool can_pop(std::string, std::string);
 		void pop();
 		void fill_expr_vect(std::string);
@@ -31,7 +33,5 @@ class expression{
 		std::string get_str_expression();
 		std::vector<std::string> get_element_type();
 		std::vector<std::string> get_expression();
-		int get_operator_priority(std::string);
-		std::string get_neutral_element(std::string);
 		float evaluate();
 };
